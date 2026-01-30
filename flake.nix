8b38f3bf70188c3d5c70aefafd8b814959d0b145
vv01f <vv01f@users.noreply.github.com>
2026-01-30 19:55:56 +0100

    flake checks out now

--- flake.nix
+++ flake.nix
@@ -27,6 +27,21 @@
         apps.zap = {
           type = "app";
           program = "${self.packages.${system}.zap}/bin/zap";
+          meta = {
+            description = "Lightning Address to BOLT11 invoice generator on CLI with PySide6 GUI.";
+            longDescription = ''
+              This GUI and CLI Tool allows user to derive a BOLT11 Invoice from
+              by passing a Lightning Address and desired parameters for a better
+              UX with Lightning Payments.
+            '';
+            homepage = "https://github.com/vv01f/lightning-address-invoice-generator";
+            source = "https://github.com/vv01f/lightning-address-invoice-generator.git";
+            bugReports = "https://github.com/vv01f/lightning-address-invoice-generator/issues";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "zap";
+            platforms = pkgs.lib.platforms.unix; # not tested: windows for lib.platforms.all
+            maintainers = with pkgs.lib.maintainers; [ "vv01f" ];
+          };
         };
 
         devShells.default = pkgs.mkShell {
